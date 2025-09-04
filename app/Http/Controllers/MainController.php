0382470602dea935a64d7b75fa93b8da2196f7d1
josehenriqueSM <jhenriquemacedo99@gmail.com>
2025-09-03 22:00:16 -0300

    ajuste

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class MainController extends Controller
+{
+    public function home(): View
+    {
+        return view('home');
+    }
+
+    public function generateExercises(Request $request)
+    {
+        echo 'Gerar exercícios';
+    }
+    public function printExercises()
+    {
+        echo 'Imprimir exercícios no navegador';
+    }
+
+    public function exportExercises() {
+        echo 'Exportar exercícios';
+    }
+}
